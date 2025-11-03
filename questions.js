7ba879016760d7fefa29e501994c86ad6c9b4e1a
sannehrobinm-rgb <sanneh.robin@gmail.com>
2025-11-03 14:04:00 +0100

    nom fichier changer/ lier entre HTML JS GAME

--- questions.js
+++ questions.js
@@ -1,8 +1,22 @@
 export const quiz_ = {
   questions: [
   {
-    q: "?",
-    answers: ["", "", " "],
+    q: "Q1?",
+    answers: ["R1", "R2", "R3"],
+    correct: 0
+  },
+  {
+    q: "Q2?",
+    answers: ["R1", "R2", "R3"],
+    correct: 0
+  },{
+    q: "Q3?",
+    answers: ["R1", "R2", "R3"],
+    correct: 0
+  },
+  {
+    q: "Q4?",
+    answers: ["R1", "R2", "R3"],
     correct: 0
   }
 ]};
