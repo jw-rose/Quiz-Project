27413a3151c5a63a20338e9de7faa69981bf7062
sannehrobinm-rgb <sanneh.robin@gmail.com>
2025-11-03 11:24:42 +0100

    ajouter element

---
+++ script.js
@@ -0,0 +1,7 @@
+const questionBox = document.getElementById('questionBox');
+const optionsEl = document.getElementById('options');
+const feedback = document.getElementById('feedback');
+const nextBtn = document.getElementById('nextBtn');
+const scoreDisplay = document.getElementById('scoreDisplay');
+const toggleTitle = document.getElementById('toggleTitle'); gi
+const hiddenText = document.getElementById('hiddenText'); 
