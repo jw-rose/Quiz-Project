8cd308468e53037d33ac4f252661c279f37b88d0
sannehrobinm-rgb <sanneh.robin@gmail.com>
2025-11-05 11:51:13 +0100

    ajout question reponse Quiz

--- questions.js
+++ questions.js
@@ -2,43 +2,43 @@ export const quiz = {
   
   q: [
   {
-    q: "Q1?",
-    option: ["R1", "R2", "R3", "R4"],
-    correct: "R2"
+    q: "Question 1 : Les débuts de Mario; Quel était le nom original de Mario avant qu’il ne devienne le célèbre plombier ?",
+    option: ["Mario Ball Z", "Goldorak", "Pac-Man", "JUMPMAN"],
+    correct: "JUMPMAN"
   },
   {
-    q: "Q2?",
-    option: ["R1", "R2", "R3", "R4"],
-    correct: "R1"
+    q: "Question 2 : Le tout premier jeu; Dans Donkey Kong (1981), quel personnage a été conçu pour sauver Pauline ?",
+    option: ["RMario", "Peach", "Chantal Goya", "Toi"],
+    correct: "Mario"
   },{
-    q: "Q3?",
-    option: ["R1", "R2", "R3", "R4"],
-    correct: "R3"
+    q: "Question 3 : Le compagnon emblématique; Quel est le nom du frère de Mario, souvent en deuxième joueur dans les jeux ?",
+    option: ["Wario", "Luigi", "Toad", "Bowser"],
+    correct: "Luigi"
   },
   {
-    q: "Q4?",
-    option: ["R1", "R2", "R3", "R4"],
-    correct: "R2"
+    q: "Question 4 : Le monde de Mario; Comment s’appelle le royaume où vivent Mario, Peach et Toad ?",
+    option: ["Royaume des fleurs", "Royaume des nuages", "Royaume Champignon ", "Royaume Koopa"],
+    correct: "Royaume Champignon "
   },
   {
-    q: "Q5?",
-    option: ["R1", "R2", "R3", "R4"],
-    correct: "R2"
+    q: "Question 5 : Les pouvoirs de Mario; Quel est le nom du pouvoir qui permet à Mario de lancer des boules de feu ?",
+    option: ["Feuille Tanooki", "Champignon doré", "Super étoile", "Fleur de feu"],
+    correct: "Fleur de feu"
   },
   {
-    q: "Q6?",
-    option: ["R1", "R2", "R3", "R4"],
-    correct: "R2"
+    q: "Question 6 : Les ennemis; Quel est le nom du personnage qui lance des marteaux et fait peur à Mario ?",
+    option: ["Luma ", "Frère Marto(Hammer Bros)", "Boo", "Dry Bones"],
+    correct: "Frère Marto (Hammer Bros)"
   },
   {
-    q: "Q7?",
-    option: ["R1", "R2", "R3", "R4"],
-    correct: "R2"
+    q: "Question 7 : Les jeux marquants; Dans quel jeu Mario a-t-il dû nettoyer une île couverte de graffitis à l’aide d’un arrosoir ?",
+    option: ["Super Mario Sunshine", "Mario galaxy", "Mario Univers", "Mario Kart"],
+    correct: "Super Mario Sunshine"
   },
   {
     q: "Q8?",
-    option: ["R1", "R2", "R3", "R4"],
-    correct: "R2"
+    option: ["Super Smash Bros", "Mario Kart 8", "Super Mario 3D World", "Donkey Kong Country"],
+    correct: "Super Mario 3D World"
   },
 
   
